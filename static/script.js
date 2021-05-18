fcdc519f04b9c8c28d0b6d28d93b915f90180f30
Taylor Thompson <taylor.thompson@epicgames.com>
2021-05-18 11:27:47 +0300

    Adds keyboard shortcuts for linking to the current page

--- static/script.js
+++ static/script.js
@@ -1,77 +1,91 @@
-function moveCaretToEnd(el) {
-  if (typeof el.selectionStart == "number") {
-    el.selectionStart = el.selectionEnd = el.value.length;
-  } else if (typeof el.createTextRange != "undefined") {
-    el.focus();
-    var range = el.createTextRange();
-    range.collapse(false);
-    range.select();
+(function () {
+  function moveCaretToEnd(el) {
+    if (typeof el.selectionStart == "number") {
+      el.selectionStart = el.selectionEnd = el.value.length;
+    } else if (typeof el.createTextRange != "undefined") {
+      el.focus();
+      var range = el.createTextRange();
+      range.collapse(false);
+      range.select();
+    }
   }
-}
 
-function moveCaretToStart(el) {
-  if (typeof el.selectionStart == "number") {
-    el.selectionStart = el.selectionEnd = 0;
-  } else if (typeof el.createTextRange != "undefined") {
-    el.focus();
-    var range = el.createTextRange();
-    range.collapse(false);
-    range.select();
+  function moveCaretToStart(el) {
+    if (typeof el.selectionStart == "number") {
+      el.selectionStart = el.selectionEnd = 0;
+    } else if (typeof el.createTextRange != "undefined") {
+      el.focus();
+      var range = el.createTextRange();
+      range.collapse(false);
+      range.select();
+    }
   }
-}
 
-function edit() {
-  const editElement = document.getElementById("edit");
-  if (editElement) {
-    // sometimes the page might not be editable
-    editElement.checked = true;
+  function edit() {
+    const editElement = document.getElementById("edit");
+    if (editElement) {
+      // sometimes the page might not be editable
+      editElement.checked = true;
+    }
   }
-}
 
-function search() {
-  window.location.pathname = "/search";
-}
+  function search() {
+    window.location.pathname = "/search";
+  }
 
-function jumpNew() {
-  window.location.pathname = "/new";
-}
+  function jumpNew() {
+    window.location.pathname = "/new";
+  }
 
-const textarea = document.getElementById("body");
-if (textarea) {
-  textarea.onkeydown = function (e) {
+  function jumpLink() {
+    const url = new URL(`/new?linkto=${buildLinkTo()}`, window.location.origin);
+    window.location.href = url;
+  }
+
+  const textarea = document.getElementById("body");
+  if (textarea) {
+    textarea.onkeydown = function (e) {
+      switch (e.key) {
+        case "Home":
+          moveCaretToStart(textarea);
+          break;
+        case "End":
+          moveCaretToEnd(textarea);
+          break;
+        default:
+          break;
+      }
+    };
+  }
+  document.onkeydown = function (e) {
+    if (e.target !== document.body) return;
     switch (e.key) {
-      case "Home":
-        moveCaretToStart(textarea);
+      case "e":
+        edit();
         break;
-      case "End":
-        moveCaretToEnd(textarea);
+      case "/":
+        search();
         break;
+      case "n":
+        jumpNew();
+        break;
+      case "l":
+        jumpLink();
       default:
         break;
     }
   };
-}
-document.onkeydown = function (e) {
-  if (e.target !== document.body) return;
-  switch (e.key) {
-    case "e":
-      edit();
-      break;
-    case "/":
-      search();
-      break;
-    case "n":
-      jumpNew();
-      break;
-    default:
-      break;
+
+  function buildLinkTo() {
+    // Remove leading '/'
+    return window.location.pathname.slice(1);
   }
-};
 
-function replaceLinkTo() {
-  const linkTo = document.getElementById("linkto");
-  if (linkTo) return;
-  linkTo.href = `/new?linkto=${window.location.pathname.slice(1)}`;
-}
+  function replaceLinkTo() {
+    const linkTo = document.getElementById("linkto");
+    if (!linkTo) return;
+    linkTo.href = `/new?linkto=${buildLinkTo()}`;
+  }
 
-replaceLinkTo();
+  replaceLinkTo();
+})();
