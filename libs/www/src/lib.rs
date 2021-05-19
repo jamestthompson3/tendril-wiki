306f6c5af7595bd1bd4156103dff3dd2d3a182ad
taylor <taylor@teukka.tech>
2021-05-19 12:35:45 +0300

    Loads user defined stylesheet from the config dir
    This allows users to customize the look and feel of their wiki :)

--- libs/www/src/lib.rs
+++ libs/www/src/lib.rs
@@ -1,3 +1,4 @@
+use build::get_config_location;
 use ::build::{config::General, RefBuilder};
 use std::sync::Arc;
 use warp::Filter;
@@ -8,6 +9,8 @@ pub mod services;
 use crate::handlers::*;
 
 pub async fn server(config: General, ref_builder: RefBuilder) {
+    let (config_dir, _) = get_config_location();
+    let user_stylesheet = config_dir.join("userstyles.css");
     let wiki_location = Arc::new(config.wiki_location);
     let index = index(config.user);
     let wiki = wiki(ref_builder.clone(), wiki_location.clone());
@@ -15,7 +18,9 @@ pub async fn server(config: General, ref_builder: RefBuilder) {
     let new_page = new_page();
     let search_page = search_page();
     let handle_search = search_handler(wiki_location.clone());
-    let static_files = warp::path("static").and(warp::fs::dir("static"));
+    let static_files = warp::path("static")
+        .and(warp::fs::dir("static"))
+        .or(warp::path("config").and(warp::fs::file(user_stylesheet)));
     let edit = edit_handler(ref_builder, wiki_location);
     // Order matters!!
     let routes = static_files
