8fb78454bde0bc0053b02ede70118c7f54f7a711
taylor <taylor@teukka.tech>
2021-05-28 14:04:32 +0300

    Adds basic html test, prep for working on transclusions

--- libs/markdown/src/parsers/html.rs
+++ libs/markdown/src/parsers/html.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use pulldown_cmark::{html, Event, Options, Parser};
 use urlencoding::encode;
 
@@ -141,5 +143,16 @@ mod tests {
         );
         let wiki_page = "My Cool Page";
         assert_eq!(String::from("/My%20Cool%20Page"), format_links(&wiki_page));
+    }
+    #[test]
+    fn parses_md_to_html_with_wikilinks() {
+        let test_string = "# Title\n [[Some Page]]. Another thing\n * Hi\n * List\n * Output";
+        let test_html = Html {
+            outlinks: vec!["Some Page".into()],
+            body: "<h1>Title</h1>\n<p><a href=\"/Some%20Page\">Some Page</a>. Another thing</p>\n<ul>\n<li>Hi</li>\n<li>List</li>\n<li>Output</li>\n</ul>\n".into()
+        };
+        let parsed = to_html(test_string);
+        assert_eq!(parsed.outlinks, test_html.outlinks);
+        assert_eq!(parsed.body, test_html.body);
     }
 }
