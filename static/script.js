20b9d1c88853632cc0779f7fd80110c1d4221b24
taylor <taylor@teukka.tech>
2021-06-04 14:48:30 +0300

    Adds JS listener for paste image events in the wiki editor

--- static/script.js
+++ static/script.js
@@ -95,3 +95,44 @@
 
   replaceLinkTo();
 })();
+
+function paster(event) {
+  const items = (event.clipboardData || event.originalEvent.clipboardData)
+    .items;
+  for (let index in items) {
+    const item = items[index];
+    if (item.kind === "file") {
+      // we need to get the filename, and `getAsFile` clobbers this with a generic name
+      // so we can just use FormData here.
+      const formData = new FormData();
+      const file = item.getAsFile();
+      const extension = file.type.split("image/").find(Boolean);
+      formData.append(
+        "file",
+        item.getAsFile(),
+        `image-${new Date().valueOf()}.${extension}`
+      );
+      const blob = formData.get("file");
+      console.log(blob);
+      fetch("/files", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/octet-stream",
+          Filename: `${blob.name}`,
+        },
+        body: blob,
+      })
+        .then(() => {
+          event.target.value += `![${blob.name} *alt text*](/files/${blob.name}) "image title"`;
+        })
+        .catch((e) => {
+          console.error(e);
+          event.target.value += "Failed to upload image";
+        });
+    }
+  }
+}
+
+let textarea = document.getElementById("body");
+
+textarea.addEventListener("paste", paster);
