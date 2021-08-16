4e7e94f2c4539a5c034a75ea84f1ae00e401d163
taylor <taylor@teukka.tech>
2021-08-16 21:32:35 +0300

    Handles cases where term is matched in the middle of a word.
    Also writes some tests, because, you know, tests can be good :p

--- libs/tasks/src/search.rs
+++ libs/tasks/src/search.rs
@@ -33,16 +33,12 @@ pub fn context_search(term: &str, wiki_location: &str) -> Vec<SearchResult> {
                     entry.path(),
                     UTF8(|_, matches| {
                         let match_str = matches.to_string();
-                        let mut match_vec: Vec<&str> = match_str.split(' ').collect();
-                        let matched_idx = match_vec.iter().position(|i| i.contains(term));
+                        let match_vec: Vec<&str> = match_str.split(' ').collect();
+                        let matched_idx = match_vec
+                            .iter()
+                            .position(|i| i.to_lowercase().contains(&term.to_lowercase()));
                         if let Some(idx) = matched_idx {
-                            match_vec.insert(idx, "<mark>");
-                            match_vec.insert(idx + 2, "</mark>");
-                            let matched_words = match_vec.join(" ");
-                            let result = SearchResult {
-                                location: name.strip_suffix(".md").unwrap().to_owned(),
-                                matched_text: matched_words,
-                            };
+                            let result = highlight_term(match_vec, idx, &term.to_lowercase(), name);
                             results.push(result);
                         } else {
                             let result = SearchResult {
@@ -58,6 +54,34 @@ pub fn context_search(term: &str, wiki_location: &str) -> Vec<SearchResult> {
         };
     }
     results
+}
+
+fn highlight_term(mut match_vec: Vec<&str>, idx: usize, term: &str, name: &str) -> SearchResult {
+    let location = name.strip_suffix(".md").unwrap().to_owned();
+    let matched_string = match_vec[idx];
+    if matched_string.to_lowercase() != term {
+        // frustration
+        let string_parts: Vec<&str> = matched_string.split(term).collect();
+        // ["f", "ration"]
+        let recombinated_string = string_parts.join(&format!("<mark>{}</mark>", term));
+        // bleh. rethink later
+        match_vec.remove(idx);
+        let mut stringified = match_vec;
+        stringified.insert(idx, &recombinated_string);
+        let matched_text = stringified.join(" ");
+        SearchResult {
+            location,
+            matched_text,
+        }
+    } else {
+        match_vec.insert(idx, "<mark>");
+        match_vec.insert(idx + 2, "</mark>");
+        let matched_text = match_vec.join(" ");
+        SearchResult {
+            location,
+            matched_text,
+        }
+    }
 }
 
 // Search for wiki by title
@@ -79,4 +103,39 @@ pub fn search(term: &str, wiki_location: &str) -> Vec<String> {
         }
     }
     found_files
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn highlights_exact_term() {
+        let matches = vec!["rust", "is", "a", "compiled", "programming", "language"];
+        let idx = 0;
+        let term = "rust";
+        let name = "learn_rust.md";
+        let result = highlight_term(matches, idx, term, name);
+        let expected = SearchResult {
+            location: String::from("learn_rust"),
+            matched_text: String::from("<mark> rust </mark> is a compiled programming language"),
+        };
+        assert_eq!(result.location, expected.location);
+        assert_eq!(result.matched_text, expected.matched_text);
+    }
+
+    #[test]
+    fn highlights_context_term() {
+        let matches = vec!["trust", "is", "important", "when", "building", "a", "team"];
+        let idx = 0;
+        let term = "rust";
+        let name = "team_building.md";
+        let result = highlight_term(matches, idx, term, name);
+        let expected = SearchResult {
+            location: String::from("team_building"),
+            matched_text: String::from("t<mark>rust</mark> is important when building a team"),
+        };
+        assert_eq!(result.location, expected.location);
+        assert_eq!(result.matched_text, expected.matched_text);
+    }
 }
