ad09942c87fb17366227a78e0e68cb46facf453f
taylor <taylor@teukka.tech>
2022-03-06 16:36:18 +0200

    Improve todo rendering and inline linking

--- libs/render/src/tasks_page.rs
+++ libs/render/src/tasks_page.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use todo::Task;
 
 use crate::{get_template_file, render_includes, Render};
@@ -13,57 +11,11 @@ impl TasksPage {
         Self { tasks: entries }
     }
     fn render_tasks(&self) -> String {
-        let mut entry_list = String::new();
-        for task in &self.tasks {
-            let status = self.format_status(&task.completed);
-            let priority = task
-                .priority
-                .to_owned()
-                .unwrap_or_else(|| String::with_capacity(0));
-            let created = task
-                .created
-                .to_owned()
-                .unwrap_or_else(|| String::with_capacity(0));
-            let project = self.format_contextual_data(&task.project);
-            let context = self.format_contextual_data(&task.context);
-            let metadata = self.format_metadata(&task.metadata);
-            let table_html = format!(
-                r#"<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>"#,
-                status, created, priority, task.body, project, context, metadata
-            );
-            entry_list.push_str(&table_html);
+        let mut html = String::new();
+        for entry in &self.tasks {
+            html.push_str(&entry.to_html());
         }
-        entry_list
-    }
-    fn format_status(&self, completed: &(bool, Option<String>)) -> String {
-        let (done, date) = completed;
-        if let true = done {
-            if date.is_some() {
-                format!("✅ {}", date.as_ref().unwrap())
-            } else {
-                "✅".into()
-            }
-        } else {
-            String::with_capacity(0)
-        }
-    }
-    fn format_contextual_data(&self, context: &[String]) -> String {
-        context
-            .iter()
-            .fold(String::new(), |mut formatted_str, ctx| {
-                let ctx_string = format!("<a href=\"{}\" target=\"_blank\">{}</a>", ctx, ctx);
-                formatted_str.push_str(&ctx_string);
-                formatted_str
-            })
-    }
-    fn format_metadata(&self, metadata: &HashMap<String, String>) -> String {
-        metadata
-            .iter()
-            .fold(String::new(), |mut formatted_str, (key, value)| {
-                let ctx_string = format!("<strong>{}:</strong> {}", key, value);
-                formatted_str.push_str(&ctx_string);
-                formatted_str
-            })
+        html
     }
 }
 
--- libs/www/src/controllers/mod.rs
+++ libs/www/src/controllers/mod.rs
@@ -2,12 +2,13 @@ use bytes::BufMut;
 use chrono::prelude::*;
 use persistance::fs::{write, write_media};
 use render::{
-    search_results_page::SearchResultsPage, uploaded_files_page::UploadedFilesPage, Render, tasks_page::TasksPage,
+    search_results_page::SearchResultsPage, tasks_page::TasksPage,
+    uploaded_files_page::UploadedFilesPage, Render,
 };
+use std::{collections::HashMap, fs::read_dir, str::FromStr, time::Instant};
+use tasks::context_search;
 use todo::Task;
 use tokio::fs;
-use std::{collections::HashMap, fs::read_dir, time::Instant};
-use tasks::context_search;
 use urlencoding::encode;
 
 use markdown::parsers::EditPageData;
@@ -212,14 +213,18 @@ pub async fn unauthorize() -> Result<impl Reply, Rejection> {
 }
 
 pub async fn get_tasks(wiki_location: String) -> Result<impl Reply, Rejection> {
-    let todo_file = fs::read_to_string(wiki_location + "todo.txt").await.unwrap();
-    let tasks = todo_file.lines().map(|l| Task::from(l)).collect::<Vec<Task>>();
+    let todo_file = fs::read_to_string(wiki_location + "todo.txt")
+        .await
+        .unwrap();
+    let tasks = todo_file
+        .lines()
+        .map(|l| Task::from_str(l).unwrap())
+        .collect::<Vec<Task>>();
     let ctx = TasksPage { tasks };
-    Ok(warp::reply::html(ctx.render(&CompileState::Dynamic)))
+    Ok(warp::reply::html(ctx.render()))
 }
 
 pub async fn edit_tasks(form_body: HashMap<String, String>) -> Result<impl Reply, Rejection> {
-
     Ok(warp::reply::with_status("ok", StatusCode::OK))
 }
 
