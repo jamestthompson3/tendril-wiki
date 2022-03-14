3bdaf6986c676c706f82c2a5aa31025853f4e014
taylor thompson <taylor@teukka.tech>
2022-03-14 13:42:45 +0200

    Better handling of EOL on Windows

--- libs/build/src/config.rs
+++ libs/build/src/config.rs
@@ -62,7 +62,7 @@ fn prompt(f: Box<dyn Fn()>) -> String {
     f();
     stdout().flush().unwrap();
     stdin.read_line(&mut response).unwrap();
-    response
+    response.trim().to_owned()
 }
 
 pub fn gen_config_interactive() -> ConfigOptions {
@@ -80,7 +80,7 @@ pub fn gen_config_interactive() -> ConfigOptions {
     }));
     let mut enable_sync = true;
     let mut git_branch: Option<String> = None;
-    match should_sync.as_str().strip_suffix('\n').unwrap() {
+    match should_sync.as_ref() {
         "y" | "t" | "true" | "yes" => {
             git_branch = Some(prompt(Box::new(|| {
                 print!("Name of branch to sync to (main): ");
@@ -92,40 +92,38 @@ pub fn gen_config_interactive() -> ConfigOptions {
         print!("Use password to protect wiki (y\\n)? ");
     }));
     let mut password: Option<String> = None;
-    match use_password.as_str().strip_suffix('\n').unwrap() {
-        "true" | "y" | "t" | "yes" | "\n" => {
-            password = Some(read_password_from_tty(Some("Password: ")).unwrap());
+    match use_password.as_ref() {
+        "true" | "y" | "t" | "yes" => {
+            password = Some(
+                read_password_from_tty(Some("Password: "))
+                    .unwrap()
+                    .to_owned(),
+            );
         }
         _ => {}
     }
     let parsed_location: PathBuf;
-    if location == "\n" {
+    if location.is_empty() {
         parsed_location = parse_location("~/wiki/");
     } else {
         parsed_location = parse_location(location.strip_suffix('\n').unwrap_or(&location));
     }
     let parsed_media_location: PathBuf;
-    if media_location == "\n" {
+    if media_location.is_empty() {
         parsed_media_location = parse_location("~/wiki_media/");
     } else {
-        parsed_media_location =
-            parse_location(media_location.strip_suffix('\n').unwrap_or(&location));
+        parsed_media_location = parse_location(&media_location);
     }
     let mut branch: Option<String> = None;
     if let Some(git_branch) = git_branch {
-        if git_branch == "\n" {
+        if git_branch.is_empty() {
             branch = Some("main".into());
         } else {
-            branch = Some(
-                git_branch
-                    .strip_suffix('\n')
-                    .unwrap_or(&git_branch)
-                    .to_owned(),
-            );
+            branch = Some(git_branch.to_owned());
         }
     }
     let user: String;
-    if username == "\n" {
+    if username.is_empty() {
         user = get_user();
     } else {
         user = username.strip_suffix('\n').unwrap_or(&username).to_owned();
