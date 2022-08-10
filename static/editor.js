8af8ecb64d1da67ef7f10666d43c0c69f39313a8
taylor thompson <taylor@teukka.tech>
2022-08-10 11:01:26 +0300

    Editor: parse wiki links on client

--- static/editor.js
+++ static/editor.js
@@ -40,8 +40,8 @@
   function setupViewer(e) {
     const div = document.createElement("div");
     div.addEventListener("click", setupEditor);
-    div.textContent = e.target.value;
-    div.innerHTML = e.target.value.replaceAll("\n", "<br />");
+    // div.textContent = e.target.value;
+    div.innerHTML = textToHtml(e.target.value);
     div.classList.add("text-block");
     if (this.value !== "" && attributesNotSet(e.target)) {
       saveBlock(div, "text/wikitext");
@@ -77,28 +77,40 @@
       el.style.height = `${scrollHeight}px`;
     }
   }
-  function getDay(dayNumber) {
-    if (dayNumber < 10) {
-      return `0${dayNumber}`;
-    }
-    return String(dayNumber);
-  }
   function saveBlock(el, type) {
-    const now = new Date();
-    el.dataset.id = now.valueOf();
     el.dataset.type = type;
-    el.dataset.created = `${now.getFullYear()}${now.getMonth() + 1}${getDay(
-      now.getDate()
-    )}${now.getHours()}${now.getMinutes()}`;
   }
   function attributesNotSet(el) {
-    if (
-      el.getAttribute("data-id") &&
-      el.getAttribute("data-type") &&
-      el.getAttribute("data-created")
-    ) {
+    if (el.getAttribute("data-type")) {
       return false;
     }
     return true;
+  }
+
+  function textToHtml(text) {
+    const linkedText = parseWikiLinks(text);
+    console.log(linkedText);
+    return linkedText;
+  }
+
+  const WIKI_LINK_REGEXP = /\[\[([a-zA-Z0-9\s?\-?(\|)?]+)\]\]/g;
+  function parseWikiLinks(text) {
+    let finalString = text;
+    for (const match of text.matchAll(WIKI_LINK_REGEXP)) {
+      const alias = match[1].split("|");
+      // handle aliased links: [[alias|actual page]]
+      if (alias.length > 1) {
+        finalString = finalString.replaceAll(
+          match[0],
+          `<a href="/${encodeURIComponent(alias[1])}">${alias[0]}</a>`
+        );
+      } else {
+        finalString = finalString.replaceAll(
+          match[0],
+          `<a href="/${encodeURIComponent(match[1])}">${match[1]}</a>`
+        );
+      }
+    }
+    return finalString;
   }
 })();
