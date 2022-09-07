b759e454ded9218a5beb63becbaaca5257783b09
taylor thompson <taylor@teukka.tech>
2022-09-08 00:06:53 +0300

    MRU: Update MRU when editing on client

--- static/mods/block-actions.js
+++ static/mods/block-actions.js
@@ -44,16 +44,31 @@ export function saveBlock() {
   })
     .then((res) => {
       if (res.status < 400) {
-        if (CURRENT_TITLE !== title) {
-          history.pushState({ name: "edit page title" }, "", title);
-          const mru = document.getElementById("mru");
-          const links = mru.querySelectorAll("a");
-          for (const link of links) {
-            if (link.href.includes(CURRENT_TITLE)) {
+        const mru = document.getElementById("mru");
+        const links = mru.querySelectorAll("a");
+        let found = false;
+        for (let i = 0; i < links.length; i++) {
+          const link = links[i];
+          if (link.href.includes(CURRENT_TITLE)) {
+            found = true;
+            if (CURRENT_TITLE !== title) {
               link.href = link.href.replace(CURRENT_TITLE, title);
               link.innerText = link.innerText.replace(CURRENT_TITLE, title);
             }
+            // use parent node here since we're targeting the list items that contain the link, not the link itself.
+            mru.insertBefore(link.parentNode, links[0].parentNode);
+            break;
           }
+        }
+        if (!found) {
+          const newEntry = document.createElement("li");
+          newEntry.innerHTML = `<a href="${title}">${title}</a>`;
+          mru.insertBefore(newEntry, mru.firstChild);
+          // Trim off the last link. List should only be 8 entries long.
+          mru.removeChild(links[7].parentNode);
+        }
+        if (CURRENT_TITLE !== title) {
+          history.pushState({ name: "edit page title" }, "", title);
           CURRENT_TITLE = title;
         }
       }
