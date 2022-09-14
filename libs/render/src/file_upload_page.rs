ef820407a5680a8b62cd7e3d41f088aeed6627d9
taylor thompson <taylor@teukka.tech>
2022-09-14 09:51:34 +0300

    FileUploader: fix UI in file upload page

--- libs/render/src/file_upload_page.rs
+++ libs/render/src/file_upload_page.rs
@@ -1,4 +1,4 @@
-use crate::{get_template_file, render_includes, Render};
+use crate::{get_template_file, render_includes, render_sidebar, Render};
 use async_trait::async_trait;
 
 pub struct FileUploader {}
@@ -18,7 +18,9 @@ impl FileUploader {
 #[async_trait]
 impl Render for FileUploader {
     async fn render(&self) -> String {
-        let ctx = get_template_file("file_upload").await.unwrap();
-        render_includes(ctx, None).await
+        let mut ctx = get_template_file("file_upload").await.unwrap();
+        let nav = get_template_file("nav").await.unwrap();
+        ctx = ctx.replace("<%= sidebar %>", &render_sidebar().await);
+        render_includes(ctx, None).await.replace("<%= nav %>", &nav)
     }
 }
